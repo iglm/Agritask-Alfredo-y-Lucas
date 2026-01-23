48abf273ee010c454670df1797c51d958518035a
Lucas Mateo Tabares Franco <mateotabares1990@gmail.com>
2026-01-23 09:42:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-83640574-35c17",
+  "appId": "1:449013529290:web:a2b0afb483c02687f6d233",
+  "apiKey": "AIzaSyBMRFmKdQ_2-Y9D12B6bUMg1RAkqZMoZNI",
+  "authDomain": "studio-83640574-35c17.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "449013529290"
+};
